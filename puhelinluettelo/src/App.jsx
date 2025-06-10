8afe7fbce5b71969bf29f5440187e1d853a1d86c
Minttu Paalanen <minttupaalanen@gmail.com>
2025-06-10 22:59:57 +0300

    exercise 2.11

--- puhelinluettelo/src/App.jsx
+++ puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Filter = ({ handleFilterChange }) => {
   return (
@@ -39,15 +40,20 @@ const Persons = ({ filterNames }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "045 1234567", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios
+    .get("http://localhost:3001/persons")
+    .then((response) => {
+      console.log("fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
