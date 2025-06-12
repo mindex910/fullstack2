f73e7dea3811d62fb42d2b80e43c457122bc9a72
Minttu Paalanen <minttupaalanen@gmail.com>
2025-06-12 12:26:15 +0300

    started 2.18

---
+++ maat/src/App.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import getCountries from './services/countries'
+
+const Countries = ({searchCountries}) => {
+  if (searchCountries.length > 10) {
+    return <div>Too many matches, specify another filter</div>
+  }
+  return searchCountries.map((country) => (
+    <div key={country.name.common}>
+      {country.name.common}
+    </div>
+  ))
+}
+
+
+const App = () => {
+  const [newFilter, setNewFilter] = useState("")
+  const [countries, setCountries] = useState([])
+
+
+useEffect(() => {
+  console.log('getting countries')
+  getCountries.getAll().then((response) => {
+    console.log('fulfilled')
+    setCountries(response.data)
+  })
+}, [])
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  const searchCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(newFilter.toLowerCase())
+  )
+
+  return (
+    <div>
+      find countries <input onChange={handleFilterChange} />
+      <Countries searchCountries={searchCountries}/>
+    </div>
+  )
+}
+
+export default App
